4cd250a17b7c855f526c0849b366c7034c24a5b9
mmulahvictor <mmulahvictor@gmail.com>
2022-08-19 08:21:56 +0300

    commit tree 42a536eee69ca47643fee4522f6274418a25b1d1

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+  attr_accessor :equilateral, :isosceles, :scalene
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    all_sides = [@side1, @side2, @side3].sort
+    if all_sides.any?{|x| x <= 0} || all_sides[0] + all_sides[1] <= all_sides[2]
+      begin
+        raise TriangleError
+        puts TriangleError.message
+      end
+    elsif all_sides.uniq.length == 1
+      :equilateral
+    elsif all_sides.uniq.length == 2
+      :isosceles
+    elsif all_sides.uniq.length == 3 
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      'NO length available'
+    end
+  end
+
+
 end
